a0e4f076264a3cf2c397f7bcb4c195c8032ef9f7
luk-kop <luk.kopicki@gmail.com>
2026-02-01 00:43:42 +0100

    ci: init workflows

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "layer_arns" {
-  description = "ARNs of the created Lambda layers (map of layer key to ARN)"
-  value       = { for k, v in aws_lambda_layer_version.this : k => v.arn }
-}
-
-output "layer_versions" {
-  description = "Version numbers of the created Lambda layers (map of layer key to version)"
-  value       = { for k, v in aws_lambda_layer_version.this : k => v.version }
-}
